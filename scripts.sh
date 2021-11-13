780296b9a71333f4c79f142d933f8c6cf1054a40
Arvind Krishna <54891738+ArvindAROO@users.noreply.github.com>
2021-11-13 19:25:06 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -2,4 +2,4 @@ echo "starting server"
 python3 src/app.py -f sentiment -b 100
 sleep 1
 echo "starting client"
-$SPARK_HOME/bin/spark-submit src/app.py > output.txt
+$SPARK_HOME/bin/spark-submit src/app.py > src/output.txt
