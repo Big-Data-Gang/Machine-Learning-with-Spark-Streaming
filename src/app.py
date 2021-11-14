0f6ca2ce237215a3984b052b6fbf905ad8dfab58
thesuhas <suhastvs@gmail.com>
2021-11-14 17:19:58 +0530

    tried converting to df but failed :(

--- src/app.py
+++ src/app.py
@@ -4,20 +4,41 @@ TCP_IP = "localhost"
 TCP_PORT = 6100
 
 from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
 from pyspark.streaming import StreamingContext
 from pyspark.sql import Row
+import json
+from pyspark.sql.types import StructType,StructField, StringType
+
+# Dataframe Schema
+schema = StructType([
+  StructField('sentiment', StringType(), True),
+  StructField('text', StringType(), True),
+  ])
 
 
 
 def predict(tweet_text):
 	
-	sent = tweet_text.toDF()
-	print(sent)
+	sent = tweet_text.collect()
+	if len(sent) > 0:
+		# Get dictionary
+		print(json.loads(sent[0]))
+		# Create empty dataframe
+		df = spark.createDataFrame(sc.emptyRDD, schema=schema)
+
+		# Iterate and get new df
+		# for i in d:
+		# 	print(d[i])
+		# print(d)
+		# df.show()
+	else:
+		pass
 
 if __name__ == "__main__":
 	sc = SparkContext(appName="tweetStream")
 	ssc = StreamingContext(sc, batchDuration= 3)
-
+	spark = SparkSession.builder.getOrCreate()
 
 	lines = ssc.socketTextStream(TCP_IP, TCP_PORT)
 
