34ac7f1dec67d2f2d1a398308ec0179b6746e05d
thesuhas <suhastvs@gmail.com>
2021-11-19 12:42:31 +0530

    started

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,22 @@
 import sparknlp
+from sparknlp.base import *
 
 # Start sparknlp
 spark = sparknlp.start()
 # Read Sample DF
-df = spark.read.csv('./src/sentiment/sample.csv')
-df.show(truncate=False)
+df = spark.read.csv('./src/sentiment/sample.csv', header=True)
+# df.show(truncate=False)
+
+# Create document assembler to convert tweet to document type
+
+documentAssembler = DocumentAssembler()\
+                    .setInputCol('Tweet')\
+                    .setOutputCol('doc')\
+                    .setCleanupMode('shrink')
+
+doc = documentAssembler.transform(df)
+
+# See the result
+# doc.select("doc.result").show(truncate=False)
+
+# Sentence Assembler to detect sentences
