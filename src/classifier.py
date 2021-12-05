31183ac30ee2a75a2a5543d8ae669857f4de15cd
Vishruth Veerendranath <vishruth2002@gmail.com>
2021-12-05 13:00:16 +0530

    Writing batch scores to CSV

--- src/classifier.py
+++ src/classifier.py
@@ -25,7 +25,12 @@ class Classifier:
         self.data = list()
 
         # Init dfs to save to csvs
-        self.csv = None
+        # self.csv = None
+
+        if self.batch == 0:
+            with open('src/supervised.csv', 'w') as f:
+                f.write('Batch No,SGD Accuracy,NB Accuracy,PA Accuracy\n')
+                f.close()
     
     def endClassifiers(self):
         pickle.dump(self.GB_classifier, open('GB.pkl', 'wb'))
@@ -42,36 +47,50 @@ class Classifier:
         # self.gb_csv.to_csv(f'./src/performance/gb-{batches}.csv')
         # self.nb_csv.to_csv(f'./src/performance/nb-{batches}.csv')
         # self.pa_csv.to_csv(f'./src/performance/pa-{batches}.csv')
-        self.csv = pd.DataFrame(data=self.data, columns=cols)
-        self.csv.to_csv('./performance/supervised.csv', index=False)
-        self.batch += 1
+        # self.csv = pd.DataFrame(data=self.data, columns=cols)
+        # self.csv.to_csv('./performance/supervised.csv', index=False)
+        # self.batch += 1
 
     def fitSGD(self):
         self.GB_classifier.partial_fit(self.X, self.Y, np.unique(self.Y))
-        print(f"Batch {self.batch}, GB Accuracy: ", self.GB_classifier.score(self.X, self.Y))
+        score = self.GB_classifier.score(self.X, self.Y)
+        print(f"Batch {self.batch}, GB Accuracy: ", score)
         # d = {'Accuracy': self.GB_classifier.score(self.X, self.Y), 'Batch': self.batch}
         # df = pd.DataFrame(data=[[self.batch, self.GB_classifier.score(self.X, self.Y)]], columns=cols)
         # self.gb_csv = self.gb_csv.append(df)
         # print("fit one done")
-        return self.GB_classifier.score(self.X, self.Y)
+        return score
 
     def fit_NB(self):
         self.NB_classifier.partial_fit(self.X, self.Y, np.unique(self.Y))
-        print(f"Batch {self.batch}, NB Accuracy: ", self.NB_classifier.score(self.X, self.Y))
+        score = self.NB_classifier.score(self.X, self.Y)
+        print(f"Batch {self.batch}, NB Accuracy: ", score)
         # df = pd.DataFrame(data=[[self.batch, self.NB_classifier.score(self.X, self.Y)]], columns=cols)
         # self.nb_csv = self.nb_csv.append(df)
-        return self.NB_classifier.score(self.X, self.Y)
+        return score
 
     def fit_PA(self):
         self.PA_classifier.partial_fit(self.X, self.Y, np.unique(self.Y))
-        print(f"Batch {self.batch}, PA Accuracy: ", self.PA_classifier.score(self.X, self.Y))
+        score = self.PA_classifier.score(self.X, self.Y)
+        print(f"Batch {self.batch}, PA Accuracy: ", score)
         # df = pd.DataFrame(data=[[self.batch, self.PA_classifier.score(self.X, self.Y)]], columns=cols)
         # self.pa_csv = self.pa_csv.append(df)
-        return self.PA_classifier.score(self.X, self.Y)
+        return score
 
     def fit(self, X, Y):
         self.X = X
         self.Y = Y
 
-        self.data.append([self.batch, self.fitSGD(), self.fit_NB(), self.fit_PA()])
+        scoreSGD = self.fitSGD()
+        scoreNB = self.fit_NB()
+        scorePA = self.fit_PA()
+        
+        print('Hi', scoreSGD, scoreNB, scorePA)
+
+        with open('src/supervised.csv', 'a') as f:
+            print('In file')
+            f.write(f'{self.batch},{scoreSGD},{scoreNB},{scorePA}\n')
+            f.close()
+
+        # self.data.append([self.batch, self.fitSGD(), self.fit_NB(), self.fit_PA()])
         self.batch += 1
