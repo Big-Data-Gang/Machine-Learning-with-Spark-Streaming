900a10c07ef1e38184cef2ef120b129122a68ab4
thesuhas <suhastvs@gmail.com>
2021-12-06 15:28:20 +0530

    wrote test script

--- src/test.py
+++ src/test.py
@@ -11,11 +11,12 @@ from sparknlp.base import *
 import json
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-
+from sklearn.metrics import f1_score, precision_score, recall_score
 from pipeline import PreProcess
 
 import classifier
 import cluster
+import pickle
 
 classifier = classifier.Classifier()
 clustering = cluster.Clustering()
@@ -27,10 +28,54 @@ from sklearn.feature_extraction.text import HashingVectorizer, CountVectorizer
 hv = HashingVectorizer(lowercase=False, alternate_sign = False)
 cv = CountVectorizer(lowercase=False)
 
+# Load Models
+nb = pickle.load('../models/2500/NB.pkl')
+pa = pickle.load('../models/2500/PA.pkl')
+sgd = pickle.load('../models/2500/GB.pkl')
+
+# File to write
+path = '../src/performance/testing/final-scores.csv'
+# Write Headers
+with open(path, 'w') as file:
+    file.write('Batch,NB Score,NB F1,NB Precision,NB Recall,PA Score,PA F1,PA Precision,PA Recall,SGD Score,SGD F1,SGD Precision,SGD Recall\n')
+    file.close()
+
+# Batch
+batch = 0
+
 # Setting np seed to get reproducible models
 np.random.seed(5)
 
-	
+def score(X, y):
+    global batch
+    global path
+    
+    # Naive Bayes
+    nbScore = nb.score(X, y)
+    y_pred_NB = nb.predict(X)
+    nbF1 = f1_score(y, y_pred_NB)
+    nbPrec = precision_score(y, y_pred_NB)
+    nbRec = recall_score(y, y_pred_NB)
+
+    # PA
+    paScore = pa.score(X, y)
+    y_pred_pa = pa.predict(X)
+    paF1 = f1_score(y, y_pred_pa)
+    paPrec = precision_score(y, y_pred_pa)
+    paRec = recall_score(y, y_pred_pa)
+
+    # SGD
+    sgdScore = sgd.score(X, y)
+    y_pred_sgd = sgd.predict(X)
+    sgdF1 = f1_score(y, y_pred_sgd)
+    sgdPrec = precision_score(y, y_pred_sgd)
+    sgdRec = recall_score(y, y_pred_sgd)
+
+    with open(path, 'a') as file:
+        file.write(f'{batch},{nbScore},{nbF1},{nbPrec},{nbRec},{paScore},{paF1},{paPrec},{paRec},{sgdScore},{sgdF1},{sgdPrec},{sgdRec}\n')
+        file.close()
+
+
 def vectorize(df):
 	# joined_df = df.withColumn('joined', array(col('finished')))
 	joined_df = df.withColumn('joined', concat_ws(' ', col('finished')))
@@ -58,13 +103,7 @@ def process(rdd):
 
 		y = np.array(df.select('sentiment').collect())
 		y = np.reshape(y, (y.shape[0],))
-		# print(vect.shape, y.shape)
-		classifier.fit(hv, y)
-
-		# clustering.fit(cv)
-		# clustering.fit(hv, y, plot=True)
-		clustering.fit(hv, y, plot=True)
-		#df.show(truncate=False)
+        score(hv, y)
 		
 
 if __name__ == "__main__":
